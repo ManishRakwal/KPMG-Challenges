660f2228432ecca5002ca2e78227ff90ef18f19a
ManishRakwal <78402021+ManishRakwal@users.noreply.github.com>
2023-01-02 16:33:55 +0530

    Update output.tf

--- Challenge1/output.tf
+++ Challenge1/output.tf
@@ -1,15 +1,15 @@
 output "VM1" {
-  value = "${azure_virtual_machine.vm1.name} ${azure_public_ip.IP.ip_address}"
+  value = "${azurerm_virtual_machine.vm1.name} ${azure_public_ip.IP.ip_address}"
 }
 
 output "VM2" {
-  value = "${azure_virtual_machine.vm2.name} ${azure_public_ip.IP2.ip_address}"
+  value = "${azurerm_virtual_machine.vm2.name} ${azure_public_ip.IP2.ip_address}"
 }
 
 output "VM3" {
-  value = "${azure_virtual_machine.vm3.name} ${azure_public_ip.IP3.ip_address}"
+  value = "${azurerm_virtual_machine.vm3.name} ${azure_public_ip.IP3.ip_address}"
 }
 
 output "VNET" {
-  value = "${azure_virtual_network.vnet1.name}"
+  value = "${azurerm_virtual_network.vnet1.name}"
 }
